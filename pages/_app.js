dd20ff0b47d0efe3b95e7bce1f908ae10fdb0c52
Eduardo Cano <canoeduardojesus@gmail.com>
2023-01-24 16:48:30 -0300

    eliminancion de archivos y creacion de seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo'
 import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
 import "bootstrap-icons/font/bootstrap-icons.css"
 import '../styles/globals.css'
@@ -6,13 +7,17 @@ import Footer from '@/components/footer/Footer'
 import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  
+
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
   return (
     <>
+      <NextSeo 
+      title='DenveDev | Desarrollo Web'
+      description='Sitio web de DenveDev'
+      />
       <BarraDeNavegacion />
       <Component {...pageProps} />
       <Footer />
