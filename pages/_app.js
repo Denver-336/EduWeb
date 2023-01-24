0dbbac3e8446f0505e87d3a4501f58969725726a
Eduardo Cano <canoeduardojesus@gmail.com>
2023-01-24 15:46:10 -0300

    instalar bootstrap desde npm

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
+import "bootstrap-icons/font/bootstrap-icons.css"
 import '../styles/globals.css'
 import BarraDeNavegacion from "@/components/menu/BarraDeNavegacion";
 import Footer from '@/components/footer/Footer'
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return (
     <>
       <BarraDeNavegacion />
--- pages/_document.js
+++ pages/_document.js
@@ -14,12 +14,12 @@ export default function Document() {
         <title>DenveDev | Desarrollo Web</title>
         {/* favicon */}
         <link rel='shortcut icon' href='public\imagenes\json.svg' />
-        {/* bootstrap */}
+        {/* bootstrap
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
           rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
           crossorigin="anonymous" />
         {/* <!-- Iconos de Bootstrap --> */}
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"></link>
+        {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"></link> */} 
         {/* estilos de fuente */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -29,11 +29,11 @@ export default function Document() {
       <body>
         <Main />
         {/* bootstrap js */}
-        <script
+        {/* <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
           crossOrigin="anonymous"
-        />
+        /> */}
         <NextScript />
       </body>
 
