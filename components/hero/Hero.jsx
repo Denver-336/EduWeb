1d4c2b7875c44d6f09f0a0e95317d222c595c094
Eduardo Cano <canoeduardojesus@gmail.com>
2023-01-24 16:48:58 -0300

    creacion de seo

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
     <section className="hero align-items-stretch">
       <div className="hero-principal d-flex flex-column justify-content-center align-items-center">
         <Image className="hero-imagen-desarrollador rounded-circle"
-          src={perfil} alt="Foto de perfil" />
+          src={perfil} alt="Foto de perfil" placeholder="blur" blurDataURL="../../public/imagenes/json.svg"/>
         <h1 className="display-1">Hola, soy Eduardo Cano</h1>
         <h2 className="h2">Soy un desarrollador web que se especializa en crear y diseñar experiencias digitales excepcionales.</h2>
       </div>
--- pages/_document.js
+++ pages/_document.js
@@ -1,42 +1,26 @@
 import { Html, Main, NextScript, Head } from 'next/document'
 
 export default function Document() {
+
   return (
     <Html lang="es">
       <Head >
         {/* metadatos */}
         <meta charSet='utf-8' />
         <meta name='author' content='DenveDev' />
-        <meta name='description' content='Sitio web de DenveDev' />
         <meta name='keywords' content='HTML, CSS, JavaScript, React, Next.js, Bootstrap' />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* titulo */}
-        <title>DenveDev | Desarrollo Web</title>
         {/* favicon */}
         <link rel='shortcut icon' href='public\imagenes\json.svg' />
-        {/* bootstrap
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
-          rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
-          crossorigin="anonymous" />
-        {/* <!-- Iconos de Bootstrap --> */}
-        {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"></link> */} 
         {/* estilos de fuente */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Rubik+Dirt&family=Share+Tech+Mono&display=swap" rel="stylesheet" />
       </Head>
-
       <body>
         <Main />
-        {/* bootstrap js */}
-        {/* <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-          crossOrigin="anonymous"
-        /> */}
         <NextScript />
       </body>
-
     </Html>
   )
 }
