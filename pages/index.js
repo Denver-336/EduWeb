7b3386db479afdc0409f7c3497fab7191a4a5e93
Eduardo Cano <canoeduardojesus@gmail.com>
2023-01-25 23:06:52 -0300

    2 correcion

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import About from "@/components/about/About";
 import Experiencia from "@/components/experiencia/Experiencia";
 import Proyectos from "@/components/proyectos/Proyectos";
 import Testimonios from "@/components/testimonios/Testimonios";
-import Contacto from "@/components/Contacto/contacto";
+import Contacto from "@/components/contacto/Contacto";
 
 
 export default function Home() {
