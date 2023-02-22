533118a42368bac8980a9ed9ae11eb8b4e945bbe
Eduardo Cano <canoeduardojesus@gmail.com>
2023-02-22 12:42:04 -0300

    rediseño estudios

--- components/experiencia/BackEnd.jsx
+++ components/experiencia/BackEnd.jsx
@@ -1,17 +1,17 @@
-export default function BackEnd() {
+export default function BackEnd () {
   return (
-    <div className="columna col-12 col-md-6">
-      <i className="bi bi-pc"></i>
-      <p className="experiencia-titulo">Desarrollo web Back-End</p>
+    <div className='columna col-12 col-md-6'>
+      <i className='bi bi-pc' />
+      <p className='experiencia-titulo'>Desarrollo web Back-End</p>
       <p>El desarrollo backend alude a aquella parte de la programación que no es visible para el usuario. Esta parte no se ejecuta en el navegador o dispositivo del cliente, sino en un servidor (físico o en la nube).</p>
-      <div className="badges-contenedor">
+      <div className='badges-contenedor'>
         <p>Frameworks:</p>
-        <span className="badge text-bg-primary">Next.js</span>
-        <span className="badge text-bg-primary">Express.js</span>
+        <span className='badge'>Next.js</span>
+        <span className='badge'>Express.js</span>
         <p>Entorno de ejecución:</p>
-        <span className="badge text-bg-primary">Node.js</span>
+        <span className='badge'>Node.js</span>
         <p>Base de datos NoSQL:</p>
-        <span className="badge text-bg-primary">MongoDB</span>
+        <span className='badge'>MongoDB</span>
       </div>
     </div>
   )
--- components/experiencia/FrontEnd.jsx
+++ components/experiencia/FrontEnd.jsx
@@ -1,20 +1,20 @@
-export default function FrontEnd() {
+export default function FrontEnd () {
   return (
-    <div className="columna col-12 col-md-6">
-      <i className="bi bi-laptop"></i>
-      <p className="experiencia-titulo">Desarrollo web Front-end</p>
+    <div className='columna col-12 col-md-6'>
+      <i className='bi bi-laptop' />
+      <p className='experiencia-titulo'>Desarrollo web Front-end</p>
       <p>El desarrollo web Front-end consiste en la conversión de datos en una interfaz gráfica para que el usuario pueda ver e interactuar con la información de forma digital usando HTML, CSS y JavaScript.</p>
-      <div className="badges-contenedor">
+      <div className='badges-contenedor'>
         <p>Lenguajes:</p>
-        <span className="badge text-bg-primary">HTML</span>
-        <span className="badge text-bg-primary">CSS</span>
-        <span className="badge text-bg-primary">JavaScript</span>
-        <span className="badge text-bg-primary">Sass</span>
+        <span className='badge'>HTML</span>
+        <span className='badge'>CSS</span>
+        <span className='badge'>JavaScript</span>
+        <span className='badge'>Sass</span>
         <p>Librerías:</p>
-        <span className="badge text-bg-primary">Bootstrap</span>
-        <span className="badge text-bg-primary">React</span>
+        <span className='badge'>Bootstrap</span>
+        <span className='badge'>React</span>
         <p>Frameworks:</p>
-        <span className="badge text-bg-primary">Vue.js</span>
+        <span className='badge'>Vue.js</span>
       </div>
     </div>
   )
