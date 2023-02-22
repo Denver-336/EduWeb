6b60bb3b8941dc7efddae4e942dff8c520a20b77
Eduardo Cano <canoeduardojesus@gmail.com>
2023-02-22 19:29:33 -0300

    rediseñe contacto y agrege formulario para mail

--- components/contacto/Contacto.jsx
+++ components/contacto/Contacto.jsx
@@ -2,39 +2,74 @@ import Link from 'next/link'
 
 export default function Contacto () {
   return (
-    <section id='contacto' className='contacto seccion-oscura'>
-      <div className='container'>
-        <div className='container text-center rectangulo d-flex justify-content-evenly'>
-          <div className='row'>
-            <div className='col-12 col-md-4 seccion-titulo'>
-              ¡Escríbeme!
-            </div>
-            <div className='col-12 col-md-4 descripcion'>
-              Contáctame para iniciar tu proyecto de desarrollo web y haré que tu visión se vuelva realidad.
-            </div>
-            <div className='col-12 col-md-4'>
-              <br />
-              <Link href='mailto:canoeduardojesus@gmail.com' className='link-light fw-bold'>
-                canoeduardojesus@gmail.com
-              </Link><br /><br />
-              <div className='iconos-redes-sociales d-flex flex-wrap align-items-center justify-content-center'>
-                <Link href='https://mail.google.com/mail' target='_blank' rel='noopener noreferrer' className='link-light'>
-                  <i className='bi bi-envelope' />
-                </Link>
-                <Link
-                  href='https://www.linkedin.com/in/canoeduardo/' target='_blank' rel='noopener noreferrer'
-                  className='link-light'
-                >
-                  <i className='bi bi-linkedin' />
-                </Link>
-                <Link href='https://github.com/Denver-336' target='_blank' rel='noopener noreferrer' className='link-light'>
-                  <i className='bi bi-github' />
-                </Link>
+    <>
+      <section id='contacto' className='contacto seccion-oscura'>
+        <div className='container'>
+          <div className='container text-center rectangulo d-flex justify-content-evenly'>
+            <div className='row'>
+              <div className='col-12 col-md-4 seccion-titulo'>
+                {/* // <!-- Button trigger modal --> */}
+                <button type='button' className='btn btn-primary' data-bs-toggle='modal' data-bs-target='#exampleModal'>
+                  ¡Escríbeme!
+                </button>
+              </div>
+              <div className='col-12 col-md-4 descripcion'>
+                Contáctame para iniciar tu proyecto de desarrollo web y haré que tu visión se vuelva realidad.
+              </div>
+              <div className='col-12 col-md-4'>
+                <br />
+                <div className='iconos-redes-sociales d-flex flex-wrap align-items-center justify-content-center'>
+                  <a className='link-light' type='button' data-bs-toggle='modal' data-bs-target='#exampleModal'>
+                    <i className='bi bi-envelope-fill' />
+                  </a>
+                  <Link
+                    href='https://www.linkedin.com/in/canoeduardo/' target='_blank' rel='noopener noreferrer'
+                    className='link-light'
+                  >
+                    <i className='bi bi-linkedin' />
+                  </Link>
+                  <Link href='https://github.com/Denver-336' target='_blank' rel='noopener noreferrer' className='link-light'>
+                    <i className='bi bi-github' />
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </section>
+
+      {/* // <!-- Modal --> */}
+      <div className='modal fade' id='exampleModal' tabIndex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h5 className='modal-title' id='exampleModalLabel'>Trabajemos juntos</h5>
+              <button type='button' className='btn-close' data-bs-dismiss='modal' aria-label='Close' />
+            </div>
+            <div className='modal-body'>
+              <form id='mensaje' action='https://formsubmit.co/58460838dbbda6f0f3d986fa29b6150b' method='POST'>
+                <div className='mb-3'>
+                  <label htmlFor='exampleFormControlInput1' className='form-label'>Nombre</label>
+                  <input type='texto' className='form-control' id='exampleFormControlInput1' name='name' required />
+                </div>
+                <div className='mb-3'>
+                  <label htmlFor='exampleFormControlInput1' className='form-label'>Correo</label>
+                  <input type='email' className='form-control' id='exampleFormControlInput1' name='email' required />
+                </div>
+                <div className='mb-3'>
+                  <label htmlFor='exampleFormControlTextarea1' className='form-label'>Mensaje</label>
+                  <textarea className='form-control' id='exampleFormControlTextarea1' rows='3' name='comments' />
+                </div>
+                <input type='hidden' name='_next' value='https://edu-web-denver-336.vercel.app/' />
+                <input type='hidden' name='_captcha' value='false' />
+              </form>
+            </div>
+            <div className='modal-footer justify-content-start'>
+              <button type='submit' className='btn btn-primary' form='mensaje'>Enviar</button>
+            </div>
+          </div>
+        </div>
       </div>
-    </section>
+    </>
   )
 }
